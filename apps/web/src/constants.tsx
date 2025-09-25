486042a2902fed49da2e306d2b743c66ecd80400
Jisook <jisuk0415@naver.com>
2025-09-25 09:24:24 +0900

    빌딩샵 관련 사이트

--- apps/web/src/constants.tsx
+++ apps/web/src/constants.tsx
@@ -4,7 +4,8 @@ export const API_HOST = import.meta.env.VITE_API_HOST;
 
 export const QUERY_KEY_USER = "user";
 
-
+export const BUILDINGSHOP_URL = "http://www.빌딩샵.com/"
+export const JUNGIN_URL = "https://www.정인부동산.com/"
 export const YOUTUBE_CHANNEL_URL = "https://www.youtube.com/c/%EB%B9%8C%EB%94%A9%EC%9D%98%EC%8B%A0";
 export const KAKAO_CHANNEL_URL = "https://pf.kakao.com/_hLhIxj";
 export const NAVER_BLOG_URL = "https://blog.naver.com/godofbuilding";
