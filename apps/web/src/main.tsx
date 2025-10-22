e72b4d1624ec4a9bce59a1c27a7eb4f40c597d42
SungEun Kim <makefavor@gmail.com>
2025-10-22 15:02:05 +0900

    mui dialog border radius 변경

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,13 +4,26 @@ import './index.css'
 import App from './App.tsx'
 import { QueryClientProvider, QueryClient } from 'react-query'
 
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+const theme = createTheme({
+  components: {
+    MuiDialog: {
+      styleOverrides: {
+        paper: {
+          borderRadius: '12px', // ✅ 원하는 radius 값
+        },
+      },
+    },
+  },
+});
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
