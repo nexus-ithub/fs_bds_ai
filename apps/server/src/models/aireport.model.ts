8b8e38069bf4a2776410b33f8273801dac72acc2
SungEun Kim <makefavor@gmail.com>
2025-11-06 15:45:48 +0900

    오류 수정

--- apps/server/src/models/aireport.model.ts
+++ apps/server/src/models/aireport.model.ts
@@ -1813,7 +1813,6 @@ export class AIReportModel {
           0,
           0,
           devDetailInfo.rent.duration,
-          false,
           debug,
           devDetailInfo.debugRentInfo
         );
