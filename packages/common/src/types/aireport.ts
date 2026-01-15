14dd4d580ea8395d2feb7c6b663775e52dc7a401
SungEun Kim <makefavor@gmail.com>
2026-01-15 14:56:54 +0900

    오류 수정

--- packages/common/src/types/aireport.ts
+++ packages/common/src/types/aireport.ts
@@ -171,7 +171,7 @@ export interface DevDetailInfo {
   // 건축정보
   buildInfo: BuildInfo;
   // 리모델링 정보
-  remodelnfo: BuildInfo;
+  remodelInfo: BuildInfo;
 
   rent: ReportValue; // 임대 (미개발)
   remodel: ReportValue; // 리모델링
